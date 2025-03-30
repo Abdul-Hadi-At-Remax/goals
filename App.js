38c149672f5d0412129ceb343006aa6f9c38a236
Abdul-Hadi-At-Remax <abdulhadibinsulaimantabassum@gmail.com>
2025-03-30 12:19:14 +0500

    Styling React Native Apps
    external styling

--- App.js
+++ App.js
@@ -4,30 +4,27 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text>This app is developed by Abdul Hadi!</Text>
+        <Text style={styles.text}>This app is developed by Abdul Hadi!</Text>
       </View>
       <View>
-        <Text
-          style={{
-            margin: 16,
-            borderWidth: 2,
-            borderColor: "red",
-            padding: 16,
-          }}
-        >
-          Hello, Tap the Button
-        </Text>
+        <Text style={styles.text}>Hello</Text>
         <Button title="Tap me" />
       </View>
     </View>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    margin: 16,
+    borderWidth: 2,
+    borderColor: "blue",
+    padding: 16,
+  },
+});
